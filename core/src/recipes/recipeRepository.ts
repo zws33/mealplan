4a9d254b83ae7b9cff9049f64961ae0c888e40df
Zach Smith <zach.smith33@gmail.com>
2024-08-19 16:46:15 -0700

    Convert Recipes repository to use top level functions instead of class definition

--- core/src/recipes/recipeRepository.ts
+++ core/src/recipes/recipeRepository.ts
@@ -1,56 +1,50 @@
-import {Pool} from 'pg';
+import {RecipesDb} from './data/database';
 
-export class RecipeRepository {
-  constructor(private readonly db: Pool) {}
-  async createIngredient(ingredientData: {
-    name: string;
-    unit: string;
-    calories: number;
-    fat: number;
-    carbs: number;
-    protein: number;
-  }): Promise<{}> {
-    const client = await this.db.connect(); // Get a connection from the pool
-    try {
-      await client.query('BEGIN');
-      const queryText =
-        'INSERT INTO ingredients (ingredient_name) VALUES ($1) RETURNING *';
-      const ingredientQueryResult = await client.query(queryText, [
-        ingredientData.name,
-      ]);
-      const ingredientId = ingredientQueryResult.rows[0].ingredient_id;
-      const nutritionQuery = `
-        INSERT INTO nutrition (ingredient_id, unit, calories, fat, carbs, protein) 
-        VALUES ($1, $2, $3, $4, $5, $6) 
-        RETURNING *
-        `;
-      const nuturitionQueryResult = await client.query(nutritionQuery, [
-        ingredientId,
-        ingredientData.unit,
-        ingredientData.calories,
-        ingredientData.fat,
-        ingredientData.carbs,
-        ingredientData.protein,
-      ]);
-      client.query('COMMIT');
-      const newIngredient = {
-        id: ingredientId,
-        name: ingredientQueryResult.rows[0].ingredient_name,
-        unit: nuturitionQueryResult.rows[0].unit,
-        calories: nuturitionQueryResult.rows[0].calories,
-        fat: nuturitionQueryResult.rows[0].fat,
-        carbs: nuturitionQueryResult.rows[0].carbs,
-        protein: nuturitionQueryResult.rows[0].protein,
-      };
-      return newIngredient;
-    } catch (e) {
-      await client.query('ROLLBACK');
-      throw e;
-    } finally {
-      client.release();
-    }
-  }
-  static create(db: Pool): RecipeRepository {
-    return new RecipeRepository(db);
+export async function createIngredient(ingredientData: {
+  name: string;
+  unit: string;
+  calories: number;
+  fat: number;
+  carbs: number;
+  protein: number;
+}): Promise<{}> {
+  const client = await RecipesDb.connect(); // Get a connection from the pool
+  try {
+    await client.query('BEGIN');
+    const queryText =
+      'INSERT INTO ingredients (ingredient_name) VALUES ($1) RETURNING *';
+    const ingredientQueryResult = await client.query(queryText, [
+      ingredientData.name,
+    ]);
+    const ingredientId = ingredientQueryResult.rows[0].ingredient_id;
+    const nutritionQuery = `
+      INSERT INTO nutrition (ingredient_id, unit, calories, fat, carbs, protein) 
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING *
+      `;
+    const nuturitionQueryResult = await client.query(nutritionQuery, [
+      ingredientId,
+      ingredientData.unit,
+      ingredientData.calories,
+      ingredientData.fat,
+      ingredientData.carbs,
+      ingredientData.protein,
+    ]);
+    client.query('COMMIT');
+    const newIngredient = {
+      id: ingredientId,
+      name: ingredientQueryResult.rows[0].ingredient_name,
+      unit: nuturitionQueryResult.rows[0].unit,
+      calories: nuturitionQueryResult.rows[0].calories,
+      fat: nuturitionQueryResult.rows[0].fat,
+      carbs: nuturitionQueryResult.rows[0].carbs,
+      protein: nuturitionQueryResult.rows[0].protein,
+    };
+    return newIngredient;
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
   }
 }
