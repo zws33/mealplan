7c18af6b3155d0419e7747312e46381cda4a7747
Zach Smith <zach.smith33@gmail.com>
2024-08-22 11:30:41 -0700

    chore: Update database host configuration to use environment variable

--- backend/src/recipes/data/database.ts
+++ backend/src/recipes/data/database.ts
@@ -2,7 +2,7 @@ import {Pool} from 'pg';
 
 export const RecipesDb = new Pool({
   user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
+  host: process.env.DB_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
