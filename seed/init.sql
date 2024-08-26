27c8c5bdd690a3b44758e9b2f123f2088a44e5b7
Zach Smith <zach.smith33@gmail.com>
2024-08-25 23:31:05 -0400

    update db table constraints. remove unnecessary config

--- seed/init.sql
+++ seed/init.sql
@@ -21,24 +21,24 @@ CREATE TABLE recipe_ingredients (
     unit VARCHAR(50),
     quantity DECIMAL(10, 2),
     PRIMARY KEY (recipe_id, ingredient_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
-    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
+    FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id) ON DELETE CASCADE
 );
 
 CREATE TABLE nutrition (
-    ingredient_id INT,
-    unit VARCHAR(50),
-    fat DECIMAL(10, 2),
-    carbs DECIMAL(10, 2),
-    protein DECIMAL(10, 2),
+    ingredient_id INT NOT NULL,
+    unit VARCHAR(50) NOT NULL,
+    fat DECIMAL(10, 2) NOT NULL,
+    carbs DECIMAL(10, 2) NOT NULL,
+    protein DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (ingredient_id, unit),
-    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id) ON DELETE CASCADE
 );
 
 CREATE TABLE recipe_instructions (
     instruction_id SERIAL PRIMARY KEY,
-    recipe_id INT,
-    step_number INT,
-    description TEXT,
-    FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id)
+    recipe_id INT NOT NULL,
+    step_number INT NOT NULL,
+    description TEXT NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id) ON DELETE CASCADE
 );
