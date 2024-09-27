505a36b95314e28621b8a35db732c9f9b218184f
Zach Smith <zach.smith33@gmail.com>
2024-09-27 16:26:37 -0400

    delete unused util methods

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -1,14 +1,3 @@
-export function factorial(n: number): number {
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
-
-export function totalCombinations(n: number, k: number): number {
-  return factorial(n) / (factorial(k) * factorial(n - k));
-}
-
 export function notEmpty<TValue>(
   value: TValue | null | undefined
 ): value is TValue {
